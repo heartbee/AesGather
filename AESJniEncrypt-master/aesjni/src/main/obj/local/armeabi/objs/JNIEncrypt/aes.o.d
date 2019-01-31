b1879adbb43a86eafdf13ce873dd1d2f6a2ee62b
yxlDragonslayer <oookllabrjkk@gmail.com>
2019-01-31 10:38:53 +0800

    Add files via upload

---
+++ AESJniEncrypt-master/aesjni/src/main/obj/local/armeabi/objs/JNIEncrypt/aes.o.d
@@ -0,0 +1,80 @@
+/Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/obj/local/armeabi/objs/JNIEncrypt/aes.o: \
+  /Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/aes.c \
+  /Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/aes.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/log.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/stdlib.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/string.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/malloc.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/alloca.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/strings.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/stdint.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/asm/types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/types.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/memory.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/jni.h \
+  /Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/base64.h
+
+/Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/aes.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/string.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/base64.h:
---
+++ AESJniEncrypt-master/aesjni/src/main/obj/local/armeabi/objs/JNIEncrypt/checksignature.o.d
@@ -0,0 +1,26 @@
+/Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/obj/local/armeabi/objs/JNIEncrypt/checksignature.o: \
+  /Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/checksignature.c \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/string.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/malloc.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/log.h \
+  /SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/jni.h \
+  /Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/checksignature.h
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/string.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/SoftWore/ndk-build/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/yixianglin/Desktop/AES/AESJniEncrypt-master/aesjni/src/main/jni/checksignature.h:
